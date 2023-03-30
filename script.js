f79ccc7b2e64d45e941e3472ecf1730808f887e1
Muhammad Riduan <inidipuy@gmail.com>
2023-03-31 02:25:22 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuToggle = document.querySelector('.menu-toggle input')
+const nav = document.querySelector('.name ul')
+const menu = document.querySelector('.name ul li a')
+const menu1 = document.querySelector('.name ul li .tentangSaya')
+const menu2 = document.querySelector('.name ul li .sekolahSaya')
+
+menuToggle.addEventListener ('click', function() {
+    nav.classList.toggle('slide')
+})
+
+menu.addEventListener ('click', function(){
+    nav.classList.toggle('slide')
+})
+
+menu1.addEventListener ('click', function(){
+    nav.classList.toggle('slide')
+})
+
+menu2.addEventListener ('click', function(){
+    nav.classList.toggle('slide')
+})
